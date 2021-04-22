f4219c16fff3c386835c565ffeacc2ca581e46f7
venkatesh chavvakula <venkateshchavvakula198@gmail.com>
2021-04-22 18:55:13 +0530

    backup

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const browserObject = require('./lib/browser');
 const scraperController = require('./lib/pageController');
+var fs = require("fs");
 
 (async () => {
     //Start the browser and create a browser instance
-    let browserInstance = await browserObject.startBrowser();
+    // let browserInstance = await browserObject.startBrowser();
     // Pass the browser instance to the scraper controller
-    scraperController(browserInstance)
+    // scraperController(browserInstance)
+    let categories = JSON.parse(fs.readFileSync('./products.json', 'utf8'));
+    let count = 0;
+    for(let cat of categories) {
+    
+
+        for(let sub of cat.sub_categories) {
+            for(let d of sub.products) {
+                count++
+               }
+        }
+        for(let sub of cat.products) {
+            count++
+           }
+    }
+    console.log(count)
 
 })();
